455e01192f74d31552de1b169ad44e4f7d7b47f3
Eric Sanchez <ericfsod@gmail.com>
2021-06-16 18:42:32 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+function add (x, n){
+    return (x + n);
+}
+
+function subtract (x, n) {
+    return (x - n);
+}
+
+function multiply (x, n){
+    return (x * n);
+}
+
+function divide (x, n){
+    return (x / n);
+}
+
+function increment (n){
+    return ++n;
+}
+
+function decrement (n){
+    return --n;
+}
+
+function makeInt (string){
+    return parseInt(string, 10);
+}
+
+function preserveDecimal (string){
+    return parseFloat(string);
+
+}
